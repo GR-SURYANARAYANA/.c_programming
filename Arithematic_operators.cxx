3d2c8321b6fac4e2d9eebbce105ebc310cec292f
GR-SURYANARAYANA <92977488+GR-SURYANARAYANA@users.noreply.github.com>
2021-11-29 08:29:11 +0530

    Add files via upload

---
+++ Arithematic_operators.cxx
@@ -0,0 +1,14 @@
+//Arthimetic operation
+#include<stdio.h>
+int main() 
+{
+    int a=20, b=30;
+    printf("\nsum =%d", a+b);
+    printf("\nDifference=%d", a-b);
+    printf("\nMultiplication =%d", a*b);
+    printf("\nDivision=%d", a/b);
+    printf("\nReminder=%d", a%b);
+    //NOTE: HERE WE GET a/b result as 0 due to integer
+    return 0;
+}
+    
